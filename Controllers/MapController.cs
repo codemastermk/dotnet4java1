9aab9d6127f241304b441f54c844af332b4ce250
Mile <mile.stanislavov@its-sk.com.mk>
2023-08-16 20:46:23 +0200

    init commit

---
+++ Controllers/MapController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using night_life_sk.Dto.Place;
+using night_life_sk.Services;
+
+namespace night_life_sk.Controllers
+{
+    [Route("api/v1/map")]
+    [ApiController]
+    public class MapController : Controller
+    {
+        private readonly BaseMapService mapService;
+        public MapController(BaseMapService mapService)
+        {
+            this.mapService = mapService;
+        }
+        [HttpGet("coordinates")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<PlaceCoordinates>))]
+        public IActionResult GetAllPlaces() => Ok(mapService.GetAllPartyPlaces());
+
+        [HttpGet("place-on-click")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<PlaceCoordinates>))]
+        public IActionResult GetPlaceAndEventOnClick (
+            [FromQuery] double longitude,
+            [FromQuery] double latitude,
+            [FromQuery] DateTime date)
+        {
+            return mapService.GetPlaceAndEventOnClick(longitude, latitude, date);
+        }
+            
+    }
+}
---
+++ Models/PartyEvent.cs
@@ -0,0 +1,16 @@
+﻿using night_life_sk.Services.persistence;
+
+namespace night_life_sk.Models
+{
+    public class PartyEvent : IEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Genre { get; set; }
+        public double? Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? EventTime { get; set; }
+        public PartyPlace? PartyPlace { get; set; }
+    }
+}
