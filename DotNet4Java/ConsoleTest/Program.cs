06d5162d3c62f71285d96460cff3b503cd1a219f
Alex Xessable <aleksandar.ristovski@xessable.mk>
2023-09-01 17:01:46 +0200

    part 1

--- DotNet4Java/ConsoleTest/Program.cs
+++ DotNet4Java/ConsoleTest/Program.cs
@@ -1,14 +1,49 @@
-﻿namespace ConsoleTest
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleTest
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
 
-            Console.WriteLine("New branch test 4");
+           await PrintNumber(4);
+            
+           
+
+            Console.WriteLine($"step 2");
 
             Console.ReadLine();
+        }
+
+        public static void FireAndForget(Action action)
+        {
+            action();
+        }
+
+        //public static void FireAndForget(Func<Task> action)
+        //{
+        //    action();
+        //}
+
+        public static async Task<int> PrintNumber(int number)
+        {
+            await Task.Delay(500);
+            Console.WriteLine(number);
+            var text = await File.ReadAllTextAsync("somefile.txt");
+
+            return 3;
+
+        }
+
+        public static async void GetNameAsync()
+        {
+            Console.WriteLine("Staring with some long work");
+            var text = await File.ReadAllTextAsync("somefile.txt");
+
+            Console.WriteLine("Staring with some long work");
+
         }
     }
 }
