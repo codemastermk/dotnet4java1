2d75f37e6e43321f5a0cf025e1a6c7099e623ecb
Alex Xessable <aleksandar.ristovski@xessable.mk>
2023-09-03 09:57:11 +0200

    Libraries and IAsyncEnumerable

--- DotNet4Java/ConsoleTest/Program.cs
+++ DotNet4Java/ConsoleTest/Program.cs
@@ -1,12 +1,31 @@
-﻿namespace ConsoleTest
+﻿using Data;
+
+namespace ConsoleTest
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
 
             Console.WriteLine("New branch test 4");
+
+
+            var collection = Enumerable.Empty<string>();
+
+            var enumerator = collection.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+
+            var repo = new AuthorsRepository();
+
+            await foreach (var author in repo.GetAuthors())
+            {
+                Console.WriteLine($"{author.FirstName} {author.LastName}");
+            }
 
             Console.ReadLine();
         }
---
+++ DotNet4Java/Data/AuthorsRepository.cs
@@ -0,0 +1,46 @@
+﻿using Models;
+
+namespace Data
+{
+    public class AuthorsRepository
+    {
+        public async IAsyncEnumerable<Author> GetAuthors()
+        {
+            await foreach (var line in GetLines())
+            {
+                var items = line.Split(",");
+
+                yield return new Author
+                {
+                    Id = int.Parse(items[0]),
+                    FirstName = items[1],
+                    LastName = items[2],
+                };
+            }
+        }
+
+        public async IAsyncEnumerable<string> GetLines()
+        {
+            yield return "4, Name, Surname";
+            yield return "7, Second Name, Second Surname";
+
+            await foreach (var line in File.ReadLinesAsync("authorsData.txt"))
+            {
+                Console.WriteLine($"Thread: {Environment.CurrentManagedThreadId}");
+                await Task.Delay(1000);
+                yield return line;
+
+            }
+
+            //string? line;
+            //var reader = new StreamReader("authorsData.txt");
+
+            //while ((line = await reader.ReadLineAsync()) != null)
+            //{
+            //    Console.WriteLine($"Thread: {Environment.CurrentManagedThreadId}");
+            //    Task.Delay(1000).GetAwaiter().GetResult();
+            //    yield return line;
+            //}
+        }
+    }
+}
