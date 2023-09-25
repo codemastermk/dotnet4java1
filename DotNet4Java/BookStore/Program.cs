657a6f5faa7deacac64bb0db2186126e48b08ec3
Igor Ilievski <ilievski@its-sk.com.mk>
2023-09-25 13:54:24 +0200

    homework 2

--- DotNet4Java/BookStore/Program.cs
+++ DotNet4Java/BookStore/Program.cs
@@ -1,4 +1,6 @@
-﻿using ConsoleTest.Model;
+﻿using BookstoreModel.Model;
+using Newtonsoft.Json;
+using System;
 
 namespace ConsoleTest
 {
@@ -6,16 +8,27 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            BookStore bookStore = new("Literatura");
+            /*BookStore bookStore = new("Literatura");
             Author hpAuthor = new Author("J. K.", " Rowling", "She has performed adult education stuff");
             BookEdition harryPoterChamberOfSecrets = new(new HashSet<Author> { hpAuthor }, "Harry Potter and the Goblet of fire", 1231548684321);
-            bookStore.AddBook(new Book(harryPoterChamberOfSecrets, 500, bookStore));
-            foreach (Book book in bookStore.Books)
+            bookStore.AddBook(new Book(harryPoterChamberOfSecrets, 500));
+            string json = JsonConvert.SerializeObject(bookStore);
+            System.IO.File.WriteAllText("bookstore.json", json);*/
+
+            string json = System.IO.File.ReadAllText("bookstore.json");
+            BookStore bookstore = JsonConvert.DeserializeObject<BookStore>(json);
+            // pring all
+            Console.WriteLine(bookstore);
+            // pring using foreach
+            bookstore.Books.ForEach(book => Console.WriteLine(book));
+            // pring using linq select
+            bookstore.Books.Select(book =>
             {
                 Console.WriteLine(book);
-            }
-            bookStore.Books.ToList().ForEach(book => Console.WriteLine(book));
-            Dictionary<int, string> dic = new Dictionary<int, string>();
+                return book;
+            }).ToList();
+
+
         }
 
     }
---
+++ DotNet4Java/BookstoreModel/Model/BookStore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreModel.Model
+{
+
+    public record BookStore(string Name)
+    {
+        public List<Book> Books { get; private set; } = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            Books.Add(book);
+        }
+    }
+    public record Author(string FirstName, string LastName, string Description)
+    {
+        public HashSet<BookEdition> BookEditions { get; private set; } = new HashSet<BookEdition>();
+
+        public bool AddBookEdition(BookEdition edition)
+        {
+            return BookEditions.Add(edition);
+        }
+
+    }
+
+    public record BookEdition(HashSet<Author> authors, string Title, long ISBN) { }
+    public record Book(BookEdition Edition, int Price) { }
+}
