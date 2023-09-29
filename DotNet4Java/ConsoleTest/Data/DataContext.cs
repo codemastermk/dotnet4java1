1213ffd4f72551ca77a15da05c13ecea0444cd0e
Petar Stevkovski <Petar.Stevkovski@xessable.mk>
2023-09-29 16:57:24 +0200

    db context in console

---
+++ DotNet4Java/ConsoleTest/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleTest.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleTest.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-RQ4JEVV;Database=Test;Integrated Security=true; TrustServerCertificate=true;");
+        }
+    }
+}
--- DotNet4Java/ConsoleTest/Program.cs
+++ DotNet4Java/ConsoleTest/Program.cs
@@ -1,14 +1,32 @@
-﻿namespace ConsoleTest
+﻿using ConsoleTest.Data;
+using ConsoleTest.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // var context = new DataContext();
 
-            Console.WriteLine("New branch test 4");
+            // Read
+            // var result = context.Blogs.OrderBy(b => b.Url).ToList();
+            // var t = 0;
+            // Create
+            // Blog blog = new Blog { Url = "https://petar.com.mk" };
+            // context.Add(blog);
+            // context.SaveChanges();
 
-            Console.ReadLine();
+            // Update
+            // var blog = context.Blogs.First(b => b.Id == 1);
+            // context.Posts.Add(new Post { Title = "test post 1", Content = "jkhjkhjkhjksf", Blog = blog });
+            // context.SaveChanges();
+
+            // Delete
+            // var blog = context.Blogs.Find(1);
+            // context.Remove(blog);
+            // context.SaveChanges();
         }
     }
 }
