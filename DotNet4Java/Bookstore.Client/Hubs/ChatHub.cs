6bb35317bbfc2390db2a3616a6a36ac5bf64ae45
Petar Stevkovski <Petar.Stevkovski@xessable.mk>
2023-10-25 14:32:16 +0200

    blazor app added

---
+++ DotNet4Java/Bookstore.Client/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Bookstore.Client.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
