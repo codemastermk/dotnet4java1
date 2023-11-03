6c2f63b5b3ef1e222ffe041726875bda1563ed8a
Petar Stevkovski <Petar.Stevkovski@xessable.mk>
2023-11-03 17:34:31 +0100

    components library, security and identity

---
+++ DotNet4Java/Bookstore.Client/CustomAuthStateProvider.cs
@@ -0,0 +1,57 @@
+﻿using Bookstore.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace Bookstore.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly string _authType = "Custom Authentication";
+
+        private readonly IDbContextFactory<BookstoreContext> _dbContextFactory;
+
+        private readonly ProtectedLocalStorage _protectedLocalStorage;
+
+        public CustomAuthStateProvider(IDbContextFactory<BookstoreContext> dbContextFactory, ProtectedLocalStorage protectedLocalStorage)
+        {
+            _dbContextFactory = dbContextFactory;
+            _protectedLocalStorage = protectedLocalStorage;
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var user = new ClaimsPrincipal();
+            return Task.FromResult(new AuthenticationState(user));
+        }
+
+        //public async Task AuthenticateUserAsync()
+        //{
+        //    var identity = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, "Petar"), new Claim(ClaimTypes.Role, "Admin") }, _authType);
+        //    var user = new ClaimsPrincipal(identity);
+        //    NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        //}
+
+        public async Task AuthenticateUserAsync(string userName, string password)
+        {
+            await using var context = await _dbContextFactory.CreateDbContextAsync();
+            var user = new ClaimsPrincipal();
+            if(!string.IsNullOrEmpty(userName) && userName == "Petar" && !string.IsNullOrEmpty(password) && password == "Petar")
+            {
+                var identity = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, "Petar"), new Claim(ClaimTypes.Role, "SuperAdmin"), new Claim(ClaimTypes.Role, "Admin"), new Claim(ClaimTypes.Role, "User") }, _authType);
+                user = new ClaimsPrincipal(identity);       
+                await _protectedLocalStorage.SetAsync("user", "Petar, Petar");
+            }
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+
+        public async Task Logout()
+        {
+            await _protectedLocalStorage.DeleteAsync("user");
+            var user = new ClaimsPrincipal();
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+    }
+}
--- DotNet4Java/Bookstore.Client/Program.cs
+++ DotNet4Java/Bookstore.Client/Program.cs
@@ -1,10 +1,12 @@
+using Bookstore.Client;
 using Bookstore.Client.Data;
 using Bookstore.Client.Hubs;
 using Bookstore.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
+using MudBlazor;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +14,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
+builder.Services.AddMudServices(options => options.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter);
 builder.Services.AddDbContextFactory<BookstoreContext>(options => options.UseSqlServer("Server=DESKTOP-RQ4JEVV;Database=Bookstore;Integrated Security=true; TrustServerCertificate=true;"));
 builder.Services.AddResponseCompression( o =>
 {
     o.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
 });
 builder.Services.AddScoped<StateContainer>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 
 var app = builder.Build();
